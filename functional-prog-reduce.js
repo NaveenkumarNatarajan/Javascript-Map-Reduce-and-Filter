8e5013fc73ee5c9730ec9d96786492b520d5ccaa
Naveen Kumar N <NaveenkumarNatarajan@users.noreply.github.com>
2017-05-15 23:57:20 +0530

    Create functional-prog-reduce.js

---
+++ functional-prog-reduce.js
@@ -0,0 +1,25 @@
+//If you want to mutiply only the odd numbers with 2, but throw away the even numbers. Your code may look something like this:
+
+/*var numbers = [1, 2, 3, 4];
+var newNumbers = [];
+
+for(var i = 0; i < numbers.length; i++) {
+    if(numbers[i] % 2 !== 0) {
+        newNumbers[i] = numbers[i] * 2;
+    }
+}
+
+console.log("The doubled numbers are", newNumbers);*/
+
+
+//And implementing the same in functional approach using - filter
+
+var numbers = [1, 2, 3, 4];
+
+var newNumbers = numbers.filter(function(number){
+    return (number % 2 !== 0);
+}).map(function(number){
+    return number * 2;
+});
+
+console.log("The doubled numbers are", newNumbers); // [2, 6]
